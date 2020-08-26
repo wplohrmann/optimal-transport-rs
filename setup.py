cd6a3890d183165b0eece2d4fdeb147599fd1951
William Lohrmann <wlohrmann@oni.bio>
2020-08-26 19:20:59 +0100

    Rename

--- setup.py
+++ setup.py
@@ -2,7 +2,7 @@ from setuptools import setup
 from setuptools_rust import Binding, RustExtension
 
 setup(
-    name="optimal-transport-rs",
+    name="optimal-transport",
     version="1.0",
     rust_extensions=[RustExtension("optimal_transport.rust", binding=Binding.PyO3, debug=False)],
     packages=["optimal_transport"],
