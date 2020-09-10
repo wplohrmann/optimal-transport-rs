ab06f42546dc26c4a79636d6f227f5ffc4e8ab61
Marco Caselli <casellimarco7@gmail.com>
2020-09-10 18:21:27 +0100

    Return cost too

--- src/main.rs
+++ src/main.rs
@@ -5,14 +5,16 @@ fn main() {
     let a = array![1, 2, 3];
     let b = array![2, 2, 2];
     let cost = array![[0, 0, 0], [0, 0, 0], [3, 3, 0]];
-    let transport_plan = calculate_1D_ot(&a, &b, &cost);
+    let (transport_cost, transport_plan) = calculate_1D_ot(&a, &b, &cost);
     println!("from: {}", a);
     println!("to: {}", b);
     println!("transport plan:");
     println!("{:?}", transport_plan);
+    println!("transport cost:");
+    println!("{:?}", transport_cost);
 }
 
-fn calculate_1D_ot(a: &Array1::<i32>, b: &Array1::<i32>, cost: &Array2::<i32>) -> Array2::<u32> {
+fn calculate_1D_ot(a: &Array1::<i32>, b: &Array1::<i32>, cost: &Array2::<i32>) -> (i32, Array2::<u32>) {
     let mut graph = GraphBuilder::new();
 
     for (i, a_value) in a.iter().enumerate() {
@@ -20,28 +22,21 @@ fn calculate_1D_ot(a: &Array1::<i32>, b: &Array1::<i32>, cost: &Array2::<i32>) -
         graph.add_edge(Vertex::Source, a_node, Capacity(*a_value), Cost(0));
         for (j, b_value) in b.iter().enumerate() {
             let b_node = ("b", j);
+            if i == 0 {
+                graph.add_edge(b_node, Vertex::Sink, Capacity(*b_value), Cost(0));
+            }
             graph.add_edge(a_node, b_node, Capacity(*b_value), Cost(cost[[i, j]]));
         }
     }
-    for (j, b_value) in b.iter().enumerate() {
-        let b_node = ("b", j);
-        graph.add_edge(b_node, Vertex::Sink, Capacity(*b_value), Cost(0));
-    }
 
     let (flow_cost, paths) = graph.mcmf();
-    // for path in paths.iter() {
-    //     println!("{}", path.cost());
-    // }
-    // println!("flow_cost: {}, paths: {:?}", flow_cost, paths.len());
 
     let mut transport_plan: Array2::<u32> = Array::zeros(cost.raw_dim());
     for path in paths.iter() {
         let a_idx = path.vertices()[1].as_option().unwrap().1;
         let b_idx = path.vertices()[2].as_option().unwrap().1;
         transport_plan[[a_idx, b_idx]] = path.amount();
-        // println!("{:?}, {:?}", path.vertices()[1].as_option().unwrap().1, path.vertices()[2]);
-        // println!("{}", path.amount())
     }
 
-    transport_plan
+    (flow_cost, transport_plan)
 }
