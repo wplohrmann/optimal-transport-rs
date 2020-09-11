4bff560b700593598684a340be42abc5dc462bbc
William Lohrmann <wlohrmann@oni.bio>
2020-09-11 21:22:56 +0100

    Make lowercase

--- src/sinkhorn.rs
+++ src/sinkhorn.rs
@@ -7,24 +7,24 @@ pub fn sinkhorn(a: Array1< f32 >, b: Array1< f32 >, cost: Array2< f32 >, reg: f3
     let m = cost.ncols();
     let epsilon = FloatOrd(1e-8);
 
-    let mut P = cost.mapv( |x| (-x / reg).exp());
-    P /= P.sum();
+    let mut p = cost.mapv( |x| (-x / reg).exp());
+    p /= p.sum();
 
     let mut u = Array1::zeros(n);
-    while (u -P.sum_axis(Axis(1))).mapv( |x: f32| FloatOrd(x.abs())).iter().max().unwrap() > &epsilon
+    while (u -p.sum_axis(Axis(1))).mapv( |x: f32| FloatOrd(x.abs())).iter().max().unwrap() > &epsilon
     {
-        u = P.sum_axis(Axis(1));
+        u = p.sum_axis(Axis(1));
         for i in 0..n {
             for j in 0..m {
-                P[[i, j]] *= a[i] / u[i];
+                p[[i, j]] *= a[i] / u[i];
             }
         }
-        let v = P.sum_axis(Axis(0));
+        let v = p.sum_axis(Axis(0));
         for i in 0..n {
             for j in 0..m {
-                P[[i, j]] *= b[i] / v[i];
+                p[[i, j]] *= b[i] / v[i];
             }
         }
     }
-    P
+    p
 }
