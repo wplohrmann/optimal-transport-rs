be74853999b5259196387b0cf0f0c479fa0f1394
Haraman Johal <haraman.johal@oxfordni.com>
2020-09-12 19:08:29 +0100

    generate simplex tableau for f32 from ot histograms

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,14 @@
+mod simplex;
+
+use ndarray::{array};
+
 fn main() {
     println!("Hello, world!");
+
+    let from_histogram = array![1., 2., 3.];
+    let to_histogram = array![2., 2., 2.];
+
+    let simplex_tableau = simplex::ot_to_simplex(from_histogram, to_histogram);
+
+    println!("{}", simplex_tableau);
 }
--- src/simplex.rs
+++ src/simplex.rs
@@ -1,31 +1,41 @@
+use std::cmp;
 use ndarray::{Array, Array1, Array2, s};
 
-use num_traits::Float;
+// use num_traits::Float;
 
 // fn simplex_solver<T: Num>(Array2<T>: table) -> (Array2, T) {
 
 // }
 
 // for now index is position in these histograms
-fn ot_to_simplex<T: Float + std::convert::From<usize>>(from_histogram: Array1<T>, to_histogram: Array1<T>) -> Array2<T> {
+pub fn ot_to_simplex(from_histogram: Array1<f32>, to_histogram: Array1<f32>) -> Array2<f32> {
     let m: usize = from_histogram.len();
     let n: usize = to_histogram.len();
     let num_rows: usize = 1 + m + n;
-    let num_cols: usize = 1 + ((1 + m) * (1 + n));
-    let mut flow_matrix: Array2<T> = Array::zeros((num_rows, num_cols));
+    let num_cols: usize = 2 + (m * n);
+    let mut flow_matrix: Array2<f32> = Array::zeros((num_rows, num_cols));
 
-    flow_matrix[[0, 0]] = 1.into();
+    flow_matrix[[0, 0]] = 1.;
 
-    for i in 1..m {
-        for j in 1..n {
-            flow_matrix[[0, ((i - 1) * m) + j]] = (j.into() - i.into()).abs();
+    println!("{}", ((1 - 2) as f32).abs());
+
+    for i in 1..m + 1 {
+        // objective function
+        for j in 1..n + 1 {
+            flow_matrix[[0, ((i - 1) * n) + j]] = (cmp::max(j, i) - cmp::min(j, i)) as f32;
         }
-        
-        for col in 1..num_cols {
-        }
-        let active_slice = flow_matrix.slice_mut(s![[i, 1 + ((i - 1) * n): 1 + (i * n)]]);
-        active_slice.fill(1.into());
+
+        // from_histogram constraints
+        flow_matrix.slice_mut(s![i, 1 + ((i - 1) * n) .. (1 + (i * n))]).fill(1.);
+        flow_matrix[[i, num_cols - 1]] = from_histogram[i - 1];
     }
 
+    // to_histogram constraints
+    for j in 0..n {
+        for i in 0..m {
+            flow_matrix[[1 + m + j, j + 1 + (i * n)]] = 1.;
+        }
+        flow_matrix[[1 + m + j, num_cols - 1]] = to_histogram[j];
+    }
     flow_matrix
 }
